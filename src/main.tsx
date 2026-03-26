1759f308021d42e0c54ffef3de2bbe4508a0d1b9
Shawn Dsouza <shawndsouz@gmail.com>
2026-03-26 23:36:25 +0530

    feat: add polyfill for Object.hasOwn to support older Chromium in react-snap

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,11 @@
+// --- POLYFILL FOR REACT-SNAP (Older Chromium) ---
+if (!(Object as any).hasOwn) {
+  (Object as any).hasOwn = function (obj: any, prop: any) {
+    return Object.prototype.hasOwnProperty.call(obj, prop);
+  };
+}
+// ------------------------------------------------
+
 import React from 'react';
 import { hydrateRoot, createRoot } from 'react-dom/client';
 import App from './App.tsx';
